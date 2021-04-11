4b78738ab6d0cbc34575b1cf525faabe25b3511c
user <user@user-PC>
2021-04-11 12:44:49 +0530

    firdst commit

---
+++ arrays/src/arrays/AscendingSorting.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class AscendingSorting {
+
+	public static void main(String[] args) {
+		int a[]= {6,8,5,3,9,2};
+		int temp=0;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]<a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+			}
+	}}
